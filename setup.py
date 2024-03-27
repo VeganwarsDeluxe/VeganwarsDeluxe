770e4289165524d5cf4cc110081ccba8333669d4
vezono <vezono.lab@gmail.com>
2024-03-27 17:25:05 -0400

    + no way: localization!

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email='vezono@gts.org.ua',
     url='https://onedev.gts.org.ua/vezono/vegans-deluxe',
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
