e9046ab83dd8ab7f96ad3ffeeaabae94abff4ec5
vezono <vezono.lab@gmail.com>
2024-05-29 21:20:08 -0400

    log anomaly fixed

--- VegansDeluxe/core/__init__.py
+++ VegansDeluxe/core/__init__.py
@@ -17,7 +17,7 @@ from .ContentManager import ContentManager, AttachedAction, RegisterState, Regis
 from .TargetType import *
 from .Engine import *
 from .utils import *
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 localizations = str(pathlib.Path(__file__).parent.resolve().joinpath("localizations"))
 
