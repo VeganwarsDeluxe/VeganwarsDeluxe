ea2e1e0bb52ae66830ea74bcb2b8744cc11c49a7
vezono <vezono.lab@gmail.com>
2024-06-01 13:20:20 -0400

    stockpile hotfix

--- VegansDeluxe/rebuild/Skills/Stockpile.py
+++ VegansDeluxe/rebuild/Skills/Stockpile.py
@@ -21,12 +21,12 @@ def register(root_context: StateContext[Stockpile]):
 
     given = []
     for _ in range(2):
-        item = random.choice(Stockpile.item_pool)()
-        pool = list(filter(lambda i: i().id not in given, Stockpile.item_pool))
+        item = random.choice(rebuild.game_items_pool)()
+        pool = list(filter(lambda i: i().id not in given, rebuild.game_items_pool))
         pool = list(filter(lambda i: i.id not in [playerItem.id for playerItem in source.items], pool))
         if pool:
             item = random.choice(pool)()
         else:
-            random.choice(Stockpile.item_pool)()
+            random.choice(rebuild.game_items_pool)()
         given.append(item.id)
         source.items.append(item)
