0fb55baf78ac9e577df8d9d3115bd26b9ce1641d
vezono <vezono.lab@gmail.com>
2024-08-17 22:43:43 -0400

    experimenting with setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from VegansDeluxe import core
 
 setup(
-    name='VegansDeluxe',
     version=core.__version__,
-    author='vezono',
-    author_email='vezono@gts.org.ua',
-    url='https://onedev.gts.org.ua/vezono/vegans-deluxe',
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={"VegansDeluxe": [".json", "localizations"]},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.12'
 )
