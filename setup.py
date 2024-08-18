638ddd17d8e18b5de882c0d7e981b5c91f6481a4
vezono <vezono.lab@gmail.com>
2024-08-17 23:08:27 -0400

    experimenting with src

--- setup.py
+++ setup.py
@@ -1,6 +1,2 @@
 from setuptools import setup
-from VegansDeluxe import core
-
-setup(
-    version=core.__version__,
-)
+setup()
--- src/VegansDeluxe/__init__.py
+++ src/VegansDeluxe/__init__.py
@@ -1,3 +1,4 @@
 import logging
+from .core import __version__
 
 logging.info("Thank you for installing VegansDeluxe library!")
