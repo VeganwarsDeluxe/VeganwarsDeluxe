b52f1670f7497fa9207114a0b1111bcb261025f1
vezono <vezono.lab@gmail.com>
2024-09-23 21:51:29 -0400

    structure: dev version

--- src/VegansDeluxe/core/__init__.py
+++ src/VegansDeluxe/core/__init__.py
@@ -6,22 +6,23 @@ import pathlib
 
 from .Actions import *
 from .Actions.WeaponAction import *
+from .ContentManager import ContentManager, AttachedAction, RegisterState, RegisterEvent, At, Next, Every, After, \
+    RegisterItem, RegisterWeapon
+from .Context import Context, StateContext, EventContext, ActionExecutionContext
+from .Engine import *
 from .Entities import *
 from .Events import *
 from .Items import *
 from .Session import *
+from .SessionManager import SessionManager
 from .Skills import *
 from .States import *
+from .TargetType import *
 from .Translator.Translator import translator
 from .Weapons import *
-from .Context import Context, StateContext, EventContext, ActionExecutionContext
-from .SessionManager import SessionManager
-from .ContentManager import ContentManager, AttachedAction, RegisterState, RegisterEvent, At, Next, Every, After, \
-    RegisterItem, RegisterWeapon
-from .TargetType import *
-from .Engine import *
 from .utils import *
-__version__ = "1.4.6"
+
+__version__ = "1.4.6-dev.1"
 
 localizations = str(pathlib.Path(__file__).parent.resolve().joinpath("localizations"))
 
