3d786974797003c234f3ad6a81c0a2fed00402b7
vezono <vezono.lab@gmail.com>
2025-04-25 20:11:37 -0400

    feat: added Info state

--- src/VegansDeluxe/rebuild/States/Info.py
+++ src/VegansDeluxe/rebuild/States/Info.py
@@ -1,44 +1,41 @@
+from VegansDeluxe.core import AttachedAction
+from VegansDeluxe.core import Entity
+from VegansDeluxe.core import OwnOnly
 from VegansDeluxe.core import RegisterState
 from VegansDeluxe.core import Session
-from VegansDeluxe.core import StateContext, AttachedAction, Everyone, Entity
+from VegansDeluxe.core import State
+from VegansDeluxe.core import StateContext
 from VegansDeluxe.core.Actions.StateAction import InstantStateAction
 from VegansDeluxe.core.Question.Question import Question
 from VegansDeluxe.core.Question.QuestionEvents import QuestionGameEvent
-from VegansDeluxe.core.Skills.Skill import Skill
 from VegansDeluxe.core.Translator.LocalizedList import LocalizedList
 from VegansDeluxe.core.Translator.LocalizedString import ls
-from VegansDeluxe.rebuild.States.DamageThreshold import DamageThreshold
+from VegansDeluxe.rebuild import DamageThreshold
 
 
-class Visor(Skill):
-    id = 'visor'
-    name = ls("rebuild.skill.visor.name")
-    description = ls("rebuild.skill.visor.description")
-
-    def __init__(self):
-        super().__init__()
-        self.cooldown_turn = 0
+class Info(State):
+    id = 'info'
 
 
-@RegisterState(Visor)
-async def register(root_context: StateContext[Visor]):
+@RegisterState(Info)
+async def register(root_context: StateContext[Info]):
     session: Session = root_context.session
-    source = root_context.entity
+    state = root_context.state
 
 
-@AttachedAction(Visor)
-class VisorAction(InstantStateAction):
-    id = 'visor'
-    name = ls("rebuild.skill.visor.action.name")
-    target_type = Everyone()
+@AttachedAction(Info)
+class InfoAction(InstantStateAction):
+    id = 'info'
+    name = ls("rebuild.skill.info.action.name")
+    target_type = OwnOnly()
 
-    def __init__(self, session: Session, source: Entity, state: Visor):
+    def __init__(self, session: Session, source: Entity, state: Info):
         super().__init__(session, source, state)
         self.state = state
 
     @property
     def hidden(self) -> bool:
-        return self.session.turn < self.state.cooldown_turn
+        return False
 
     async def func(self, source: Entity, target: Entity):
         visor_message = Question(text=ls("rebuild.skill.visor.message").format(
@@ -65,4 +62,3 @@ class VisorAction(InstantStateAction):
         )
 
         self.state.cooldown_turn = self.session.turn + 3
-
