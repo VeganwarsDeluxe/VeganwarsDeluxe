bb4c30e8fbf80cd3da2c57f19aedd39bc9af8ab7
vezono <vezono.lab@gmail.com>
2025-04-29 13:24:33 -0400

    feat: version increment

--- src/VegansDeluxe/core/__init__.py
+++ src/VegansDeluxe/core/__init__.py
@@ -23,7 +23,7 @@ from .Translator.Translator import translator
 from .Weapons import *
 from .utils import *
 
-__version__ = "1.6.4"
+__version__ = "1.6.5"
 
 localizations = str(pathlib.Path(__file__).parent.resolve().joinpath("localizations"))
 
